150596d0ac5607c6e1f23be0bda457f9605366cb
Abel-LiuJinQuan <2971499692@qq.com>
2019-06-06 21:19:35 +0800

    添加mongodb配置

--- src/main/java/com/hita/tata/TataApplication.java
+++ src/main/java/com/hita/tata/TataApplication.java
@@ -1,9 +1,13 @@
 package com.hita.tata;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class TataApplication {
 
 	public static void main(String[] args) {
